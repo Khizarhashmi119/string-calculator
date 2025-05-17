178881fddd3cb9ca5e2b6679d58a5ebbaccb902d
Mohd. Khizar Hashmi <khizarhashmi119@gmail.com>
2025-05-17 11:54:12 +0530

    Implement handling of custom delimiters in add function

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,4 +1,17 @@
 export function add(numbers: string): number {
+  if (numbers.startsWith("//")) {
+    const indexOfFirstNewline = numbers.indexOf("\n");
+    const delimiter = numbers.slice(2, indexOfFirstNewline);
+
+    return numbers
+      .slice(indexOfFirstNewline + 1)
+      .replaceAll(delimiter, ",")
+      .replaceAll("\n", ",")
+      .split(",")
+      .map((value) => Number(value))
+      .reduce((previousValue, currentValue) => previousValue + currentValue);
+  }
+
   return numbers
     .replaceAll("\n", ",")
     .split(",")
