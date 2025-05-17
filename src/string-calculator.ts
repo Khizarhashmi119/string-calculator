222e4a52b60cf85a4d3edd04cfc75af2266df9db
Mohd. Khizar Hashmi <khizarhashmi119@gmail.com>
2025-05-17 13:12:56 +0530

    Refactor add function to streamline number parsing and improve readability

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -2,49 +2,24 @@ export function add(numbers: string): number {
   if (numbers.startsWith("//")) {
     const indexOfFirstNewline = numbers.indexOf("\n");
     const delimiter = numbers.slice(2, indexOfFirstNewline);
-
-    const number_list = numbers
-      .slice(indexOfFirstNewline + 1)
-      .replaceAll(delimiter, ",")
-      .replaceAll("\n", ",")
-      .split(",")
-      .map((value) => Number(value));
-
-    const negativeNumbers: number[] = [];
-
-    number_list.forEach((value) => {
-      if (value < 0) {
-        negativeNumbers.push(value);
-      }
-    });
-
-    if (negativeNumbers.length) {
-      throw `negative numbers not allowed ${negativeNumbers.join(", ")}`;
-    }
-
-    return number_list.reduce(
-      (previousValue, currentValue) => previousValue + currentValue,
-    );
+    numbers = numbers.slice(indexOfFirstNewline + 1).replaceAll(delimiter, ",");
   }
 
-  const number_list = numbers
+  const numberList = numbers
     .replaceAll("\n", ",")
     .split(",")
     .map((value) => Number(value));
 
   const negativeNumbers: number[] = [];
 
-  number_list.forEach((value) => {
-    if (value < 0) {
-      negativeNumbers.push(value);
-    }
+  numberList.forEach((value) => {
+    if (value < 0) negativeNumbers.push(value);
   });
 
-  if (negativeNumbers.length) {
+  if (negativeNumbers.length)
     throw `negative numbers not allowed ${negativeNumbers.join(", ")}`;
-  }
 
-  return number_list.reduce(
+  return numberList.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
   );
 }
