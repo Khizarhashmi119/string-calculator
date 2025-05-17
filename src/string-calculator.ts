2e2ee5de6f233a79262d725d24f5bec5e7aaad6e
Mohd. Khizar Hashmi <khizarhashmi119@gmail.com>
2025-05-17 11:40:11 +0530

    Replace newlines with commas in add function for better input handling

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,5 +1,6 @@
 export function add(numbers: string): number {
   return numbers
+    .replaceAll("\n", ",")
     .split(",")
     .map((value) => Number(value))
     .reduce((previousValue, currentValue) => previousValue + currentValue);
